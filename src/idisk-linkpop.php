1e6c1333521a45318b8a9f456066d0ae19659bb0
Andreas Rudolph <andy@openindex.de>
2014-11-26 02:23:27 +0100

    Feature: Try to download content via cURL, if `allow_url_fopen` is disabled.

--- src/idisk-linkpop.php
+++ src/idisk-linkpop.php
@@ -50,13 +50,44 @@ function idisk_linkpop_shortcode($atts) {
     $url .= '/table_long';
   }
 
-  // get and return the content for the URL
-  $content = implode('', file($url));
+  // get the content for the URL
+  $content = idisk_linkpop_download($url);
+  if (!is_string($content) || strlen($content) < 1) {
+    return idisk_linkpop_error('Link-Popularity konnte nicht abgerufen werden!');
+  }
+
+  // convert and return the content
   $utf8 = (isset($settings['utf8'])) ? strtolower($settings['utf8']) : null;
   return ($utf8 == '1' || $utf8 == 'true') ? utf8_encode($content) : $content;
 }
 
 function idisk_linkpop_error($msg) {
-  return '<div style="border:1px solid red; padding:1em; text-align:center; background-color:#f5f5b5; color:red;">' .
-      '<strong>Fehler:</strong> ' . $msg . '</div>';
+  return '<div style="border:1px solid red; padding:1em; text-align:center; background-color:#f5f5b5; color:red;">'
+      . '<strong>Fehler:</strong> ' . $msg . '</div>';
+}
+
+function idisk_linkpop_download($url) {
+
+  // load the content via file_get_contents,
+  // if allow_url_fopen is enabled in the PHP runtime
+  if (ini_get('allow_url_fopen')) {
+    return file_get_contents($url);
+  }
+
+  // alternatively load the content via cURL,
+  // if it is available in the PHP runtime
+  elseif (function_exists('curl_init')) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    $content = curl_exec($curl);
+    curl_close($curl);
+    return $content;
+  }
+
+  // return an error message, if the download is not possible
+  else {
+    return idisk_linkpop_error('Ihr Webspace unterstützt weder <strong>allow_url_fopen</strong> noch <strong>cURL</strong>!');
+  }
 }
