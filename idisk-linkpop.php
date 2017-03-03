f038109e249a116228db628bb3bcdc3698beea6c
Andreas Rudolph <andy@openindex.de>
2017-03-03 08:26:26 +0100

    migrated to MIT license

--- idisk-linkpop.php
+++ idisk-linkpop.php
@@ -1,14 +1,37 @@
 <?php
 /*
 Plugin Name: IDisk-Link-Popularity
-Plugin URI: http://www.immobiliendiskussion.de/
+Plugin URI: https://immobiliendiskussion.de/
 Description: Dieses Plugin integriert die Link-Popularity der ImmobilienDiskussion in WordPress.
 Version: 0.3-SNAPSHOT
 Author: Andreas Rudolph, Walter Wagner (OpenIndex.de)
 Author URI: http://www.openindex.de/
-License: GPL3
-Id: $Id$
+License: MIT
 */
+
+/**
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author Andreas Rudolph <andy@openindex.de>
+ * @copyright 2006-2017 OpenIndex.de
+ * @license MIT
+ */
 
 // Register the [IDiskLinkpopularity] shortcode.
 // see http://codex.wordpress.org/Function_Reference/add_shortcode
